6c9a057f7f0e1a4dd0b356bfd2b16d39f5eaccfa
miseonn <m1109sun@gmail.com>
2024-01-21 15:59:37 +0900

    스택

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,14 +1,47 @@
-// 선형 자료구조 - 스택
-import java.util.Stack;
-public class MyStack {
-    public static void main(String[] args) {
-        Stack stack = new Stack();
+// ArrayList를 이용한 스택 구현
 
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
-        System.out.println(stack);
+import java.util.ArrayList;
+class MyStack {
+    ArrayList list;
+
+    MyStack() {
+        this.list = new ArrayList();
+    }
+
+    public boolean isEmpty() {
+        if (this.list.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(int data) {
+        this.list.add(data);
+    }
+
+    public Integer pop() {
+        if (this.isEmpty()) {
+            System.out.println("Stack is empty!");
+            return null;
+        }
+
+        int data = (int)this.list.get(this.list.size() - 1);
+        this.list.remove(this.list.size() - 1);
+        return data;
+    }
+
+    public Integer peek() {
+        if (this.isEmpty()) {
+            System.out.println("Stack is empty!");
+            return null;
+        }
+
+        int data = (int)this.list.get(this.list.size() - 1);
+        return data;
+    }
+
+    public void printStack() {
+        System.out.println(this.list);
     }
 }
