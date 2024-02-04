6b989088dee6ee2bdf2804a634782370b040c064
miseonn <m1109sun@gmail.com>
2024-02-05 06:42:29 +0900

    정렬 연습문제2

---
+++ src/SortPractice2.java
@@ -0,0 +1,56 @@
+// Practice2
+// 문자열 배열 strs가 주어졌을 때 anagram으로 묶어서 출력하는 프로그램을 작성하세요.
+// anagram은 철자 순서를 바꾸면 같아지는 문자를 의미한다.
+// 예) elvis <> lives
+// anagram 그룹 내에서 출력 순서는 상관 없다.
+
+// 입출력 예시
+// 입력: "eat", "tea", "tan", "ate", "nat", "bat"
+// 출력: [[eat, tea, ate], [bat], [tan, nat]]
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SortPractice2 {
+    public static ArrayList<ArrayList<String>> solution(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        for (String s: strs) {
+            char[] cArr = s.toCharArray();
+            sort(cArr);
+            String key = String.valueOf(cArr); // char 문자열을 String으로 바꾸기
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void sort(char[] arr) {
+        // 삽입 정렬
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    char tmp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(solution(strs));
+
+        strs = new String[]{"abc", "bac", "xyz", "zyx", "aaa"};
+        System.out.println(solution(strs));
+    }
+}
