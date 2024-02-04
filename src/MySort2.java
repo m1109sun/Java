2b588e60bed65abc57aa5c49a4f758bce9062d10
miseonn <m1109sun@gmail.com>
2024-02-04 16:02:33 +0900

    정렬2 (합병 정렬/힙 정렬/퀵 정렬/트리 정렬)

--- src/MySort2.java
+++ src/MySort2.java
@@ -41,11 +41,89 @@ public class MySort2 {
             arr[i] = tmp[i];
         }
     }
+    // 힙 정렬
+    public static void heapSort(int[] arr) {
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            heapify(arr, i, arr.length);
+        }
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, 0, i);
+        }
+    }
+
+    public static void heapify(int[] arr, int parentIdx, int size) {
+        int leftIdx = 2 * parentIdx + 1;
+        int rightIdx = 2 * parentIdx + 2;
+        int maxIdx = parentIdx;
+
+        if (leftIdx < size && arr[maxIdx] < arr[leftIdx]) {
+            maxIdx = leftIdx;
+        }
+
+        if (rightIdx < size && arr[maxIdx] < arr[rightIdx]) {
+            maxIdx = rightIdx;
+        }
+
+        if (parentIdx != maxIdx) {
+            swap(arr, maxIdx, parentIdx);
+            heapify(arr, maxIdx, size);
+        }
+    }
+
+    public static void swap(int[] arr, int i , int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // 퀵 정렬
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int pivot = partition(arr, left, right);
+
+        quickSort(arr, left, pivot - 1);
+        quickSort(arr, pivot + 1, right);
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        int i = left;
+        int j = right;
+
+        while (i < j) {
+            while (arr[j] > pivot && i < j) {
+                j--;
+            }
+
+            while (arr[i] <= pivot && i < j) {
+                i++;
+            }
+
+            swap(arr, i, j);
+        }
+        swap(arr, left, i);
+
+        return i;
+    }
+
     public static void main(String[] args) {
         // Test code
         int[] arr = {3, 5, 2, 7, 1, 4, 6};
         int[] tmp = new int[arr.length];
         mergeSort(arr, tmp, 0, arr.length - 1);
         System.out.println("합병 정렬: " + Arrays.toString(arr));
+
+        arr = new int[]{3, 5, 2, 7 ,1, 4, 6};
+        heapSort(arr);
+        System.out.println("힙 정렬: " + Arrays.toString(arr));
+
+        arr = new int[]{3, 5, 2, 7 ,1, 4, 6};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("퀵 정렬: " + Arrays.toString(arr));
     }
 }
